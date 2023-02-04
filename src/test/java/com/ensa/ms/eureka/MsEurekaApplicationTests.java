43f1e9898016305e66c35ca05f72bbef0371ca7b
Abdelkhalak rbati <rbati.dev@gmail.com>
2023-02-05 00:26:05 +0100

    init project

---
+++ src/test/java/com/ensa/ms/eureka/MsEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ensa.ms.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsEurekaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
